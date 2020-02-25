add2895f8e0599b440d6d934ee66f6c8bb8339ef
0pippi0 <j.romany91@gmail.com>
2020-02-26 01:18:06 +0900

    Updates

---
+++ precache-manifest.9baf37a9bbc1d3c243679045172b2489.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189b6ad0c5e06422f031b38ac517a359",
+    "url": "/practice_Immutability/index.html"
+  },
+  {
+    "revision": "204a89fd553e772476ab",
+    "url": "/practice_Immutability/static/js/2.b549ff96.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/practice_Immutability/static/js/2.b549ff96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8735b845eacf29ea978",
+    "url": "/practice_Immutability/static/js/main.11dc6006.chunk.js"
+  },
+  {
+    "revision": "0473b0fe0daa8c7b4930",
+    "url": "/practice_Immutability/static/js/runtime-main.7b683e32.js"
+  }
+]);
