52f0d64dd98b8de61faf8db2a1c9e8c9a6385ac3
0pippi0 <j.romany91@gmail.com>
2020-02-26 00:39:27 +0900

    주로 사용 되는건 위 9가지
    state 내부에 하나의 Immutable 객체를 만들어두고, 상태 관리를 모두 이 객체를 통해서 진행
    
    data 라는 Map 을 만들었고, 그 내부에는 users List 가 있고, 그 안에 또 Map 두개가 안에
    onChange 부분  set 을 통해서 값을 변경
    input 값을 공백으로 만들어야 하고, users 에 새 Map 을 추가 data.set(…).update(…) 형식으로
    render 함수도 변경 Map 혹은 List 의 값을 읽을땐 data.users 이런식으로는 읽지 못하고, data.get(‘users’) 이런식

--- immutability/src/App.js
+++ immutability/src/App.js
@@ -1,44 +1,53 @@
 import React, { Component } from 'react';
 import UserList from './UserList';
+import { List } from 'immutable';
 
 
 class App extends Component {
   id = 3;
   
   state = {
-    input: '',
-    users: [
-      {
-        id: 1,
-        username: 'velopert'
-      },
-      {
-        id: 2,
-        username: 'mjkim'
-      }
-    ]
+    data: Map({
+      input: '',
+      users: List([
+        Map({
+          id: 1,
+          username: 'velopert'
+        }),
+        Map({
+          id: 2,
+          username: 'mjkim'
+        })
+      ])
+    })
   }
 
   onChange = (e) => {
     const { value } = e.target;
+    const { data } = this.state;
+
     this.setState({
-      input: value
+      data: data.set('input', value)
     });
   }
 
-  onButtonClick = (e) => {
-    this.setState(({ users, input }) => ({
-      input: '',
-      users: users.concat({
+  onButtonClick = () => {
+    const { data } = this.state;
+
+    this.setState({
+      data: data.set('input', '')
+      .update('users', users => users.push(Map({
         id: this.id++,
-        username: input
-      })
-    }))
+        username: data.get('input')
+      })))
+    })
   }
 
   render() {
     const { onChange, onButtonClick } = this;
-    const { input, users } = this.state;
+    const { data } = this.state;
+    const input = data.get('input');
+    const users = data.get('users');
 
     return (
       <div>
